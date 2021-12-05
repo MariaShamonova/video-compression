2109eb74212aefe42219dace002a9e4b6202ff2d
mshamonova <mshamonova@saber3d.ru>
2021-12-05 22:33:18 +0300

    add DCT algorithm transform coeff, quantization and zig zag scan

---
+++ DCT.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  5 13:29:30 2021
+
+@author: mariashamonova
+"""
+
+import numpy as np
+import math
+from reshapeFrame import reshapeFrame
+
+
+def ZigZagTransform(block):
+    zigzag = []
+    reverse_block = [i[::-1] for i in block[::-1]]
+
+    for index in range(1, len(block) + 1):
+        slice = [i[:index] for i in block[:index]]
+
+        diag = [slice[i][len(slice)-i-1] for i in range(len(slice))]
+
+        if len(diag) % 2:
+            diag.reverse()
+        zigzag += diag
+    for index in reversed(range(1, len(block))):
+        slice = [i[:index] for i in block[:index]]
+
+        diag = [block[len(block) - index + i][len(block) - i - 1]
+                for i in range(len(slice))]
+
+        if len(diag) % 2:
+            diag.reverse()
+        zigzag += diag
+    return zigzag
+
+
+def DCT(frame, N, QP):
+    print('DCT')
+    rows, columns = frame.shape
+
+    X, shape = reshapeFrame(frame, N)
+
+    A = np.zeros(shape)
+    Y = np.zeros(shape)
+
+    for i in range(0, shape[0]):
+        for j in range(0, shape[1]):
+            cosx = math.cos(int(((2*j+1)*i*math.pi)/2*N))
+            if (i == 0):
+                A[i][j] = math.sqrt(1/N) * cosx
+            else:
+                A[i][j] = math.sqrt(2/N) * cosx
+
+            # Quantization
+            Y[i][j] = QP * \
+                np.round(A[i][j].dot(X[i][j]).dot(A[i][j].transpose()) / QP, 0)
+            
+            Y[i][j] = ZigZagTransform(Y[i][j])
+
+    return Y
--- temp.py
+++ temp.py
@@ -5,6 +5,97 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+from DCT import DCT
+from reshapeFrame import reshapeFrame
+
+p_frame_thresh = 300000
+
+
+def degreeOfSimilarity(block1, block2):
+    # Евклидовая норма разницы двух матриц
+    diff = block1 - block2
+    np.linalg.norm(diff, ord=None, axis=None, keepdims=False)
+    print(np)
+
+
+def splitIntoDisjointBlocks(block):
+    result = block.reshape(3, 4, 4)
+    return result
+
+
+def searchForKeyframes(curr_frame, prev_frame):
+    diff = cv2.absdiff(curr_frame, prev_frame)
+    non_zero_count = np.count_nonzero(diff)
+    if (non_zero_count > p_frame_thresh):
+        print("Got P-Frame")
+        prev_frame = curr_frame
+        return "P"
+    return "I"
+    print('search key frames')
+
+
+def encodeFrame(frame):
+    print('encode')
+
+
+def setChromaticSamples(frame_y, frame):
+    r, c = int(np.array(frame).shape[0]/4), int(np.array(frame).shape[1]/4)
+    blocks = np.zeros((r, c, 2))
+
+    blocks[0, 1] = [1, 2]
+    for i in range(0, r):
+        for j in range(0, c):
+            x = 2*i + 1
+            y = 2*j+1
+            blocks[i][j] = [frame[x][y][0] - frame_y[x]
+                            [y], frame[x][y][1] - frame_y[x][y]]
+    return blocks
+
+
 if __name__ == "__main__":
 
-    print('Create project 1')
+    cap = cv2.VideoCapture('video.mp4')
+
+    i = 0
+    frames = []
+
+    while(i < 5):
+        # while(cap.isOpened()):
+
+        ret, frame = cap.read()
+
+        def frame_gray(rgb): return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+        frame_y = frame_gray(frame)
+        frame_4x4, shape_4x4 = reshapeFrame(frame_y, 4)
+
+        frame_chromatic = setChromaticSamples(
+            frame_y, frame)
+
+        plt.figure(figsize=(10, 10))
+
+        plt.imshow(frame_y, cmap=plt.get_cmap(name='gray'))
+        if ret == False:
+            break
+        # cv2.imwrite('kang'+str(i)+'.jpg', frame)
+        frames.append(frame_y)
+        i += 1
+
+    cap.release()
+
+    quanted_frame = []
+
+    for index, frame in enumerate(frames):
+
+        if (index > 0):
+            break
+            # encodeFrame(frame)
+        else:
+            quanted_frame = DCT(frame, 8, 15)
+
+    cv2.destroyAllWindows()
+
+    # 1.Разбить кадры на непересекающиеся блоки
